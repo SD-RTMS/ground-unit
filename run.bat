cdef21708e6d38cf4b258a35e1c4a90a26e8a7f3
Ezra Taimuty-Loomis <etaimutyloomis@gmail.com>
2020-10-12 14:38:41 -0400

    Add batch script to run backend

---
+++ run.bat
@@ -0,0 +1,4 @@
+docker build -t my-prometheus .
+docker run -d -p 9090:9090 --net=host my-prometheus
+docker run -d -p 3000:3000 --net=host grafana/grafana
+docker run -d -p 9100:9100 -v "/:/hostfs" --net=host prom/node-exporter --path.rootfs=/hostfs

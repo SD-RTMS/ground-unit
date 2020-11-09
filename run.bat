81e3f5adbc6b599091903e1465acd656ee27e1bd
Ezra Taimuty-Loomis <etaimutyloomis@gmail.com>
2020-11-08 19:49:42 -0500

    Update run scripts

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 docker build -t my-prometheus .
-docker run -d -p 9090:9090 --net=host my-prometheus
 docker run -d -p 3000:3000 --net=host grafana/grafana
 docker run -d -p 9100:9100 -v "/:/hostfs" --net=host prom/node-exporter --path.rootfs=/hostfs
+docker run -d -p 9091:9091 --net=host prom/pushgateway
+docker run -d -p 9090:9090 --net=host my-prometheus
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t my-prometheus .
 docker run -d -p 3000:3000 --net=host grafana/grafana
-docker run -d -p 9100:9100 -v "/:/hostfs" --net=host prom/node-exporter --path.rootfs=/hostfs --collector.textfile.directory=/hostfs/tmp
+docker run -d -p 9100:9100 -v "/:/hostfs" --net=host prom/node-exporter --path.rootfs=/hostfs
 docker run -d -p 9091:9091 --net=host prom/pushgateway
 docker run -d -p 9090:9090 --net=host my-prometheus

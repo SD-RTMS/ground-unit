df2dee62553ccae176a83e1f7bdee7ac0c375e36
Ezra Taimuty-Loomis <etaimutyloomis@gmail.com>
2020-11-09 13:34:19 -0500

    Configure data persistence

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t my-prometheus .
-docker run -d -p 3000:3000 --net=host grafana/grafana
+docker run -d -p 3000:3000 --net=host --user=0:0 --volume "$PWD/grafana:/var/lib/grafana" grafana/grafana
 docker run -d -p 9100:9100 -v "/:/hostfs" --net=host prom/node-exporter --path.rootfs=/hostfs
 docker run -d -p 9091:9091 --net=host prom/pushgateway
-docker run -d -p 9090:9090 --net=host my-prometheus
+docker run -d -p 9090:9090 --net=host --user=0:0 --volume "$PWD/prom:/prometheus" my-prometheus
